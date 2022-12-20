0abddd866edd73d4c2b4de640777b348455f4858
admiskandar <75779713+admiskandar@users.noreply.github.com>
2022-12-20 19:44:38 +0800

    Create all models

---
+++ app/Models/BulletinRecord.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BulletinRecord extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/RegistrationProfileRecord.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RegistrationProfileRecord extends Model
+{
+    use HasFactory;
+}
