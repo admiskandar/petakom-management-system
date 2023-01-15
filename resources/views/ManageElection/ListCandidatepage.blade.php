969ed12bd3b681e8d846ebedf4a556d5a7eb1a79
Alif720 <m.alifaimansmd@gmail.com>
2023-01-16 05:28:49 +0800

    Adjust ag shikit2

--- resources/views/ManageElection/ListCandidatepage.blade.php
+++ resources/views/ManageElection/ListCandidatepage.blade.php
@@ -4,32 +4,45 @@
             <div class="row">
                 <div class="col-12">
                     <br>
-                    <div class="card">                     
+                    <div class="card">
                         <div class="card-body">
+                            @if (Auth::user()->user_type == 'Student')
+                                <div style="display: none">
+                                    <a href="{{ route('election.create') }}"><button
+                                            class="btn btn-gradient-primary px-4 float-right mt-0 mb-3"><i
+                                                class="mdi mdi-plus-circle-outline mr-2"></i>Add new candidate</button>
+                                    </a>
+                                    <a href="{{ route('election.leaderboard') }}"><button
+                                            class="btn btn-gradient-primary px-4 float-right mt-0 mb-3"style="margin-right: 30px;"><i
+                                                class="mdi mdi-plus-circle-outline mr-2"></i>View Leaderboard</button>
+                                    </a>
+                                </div>
+                            @else
+                                <div>
+                                    <a href="{{ route('election.vote') }}"><button
+                                            class="btn btn-gradient-primary px-4 float-right mt-0 mb-3"style="margin-right: 30px;"><i
+                                                class="mdi mdi-plus-circle-outline mr-2"></i>Vote</button> </a>
+                                </div>
+                            @endif
 
-                            <a href="{{route('election.create')}}"><button class="btn btn-gradient-primary px-4 float-right mt-0 mb-3"><i class="mdi mdi-plus-circle-outline mr-2"></i>Add new candidate</button> </a>
-                            
-                            <a href="{{route('election.leaderboard')}}"><button class="btn btn-gradient-primary px-4 float-right mt-0 mb-3"style="margin-right: 30px;"><i class="mdi mdi-plus-circle-outline mr-2"></i>View Leaderboard</button> </a>
-
-                            <a href="{{route('election.vote')}}"><button class="btn btn-gradient-primary px-4 float-right mt-0 mb-3"style="margin-right: 30px;"><i class="mdi mdi-plus-circle-outline mr-2"></i>Vote</button> </a>
-
-                            <h4 class="header-title mt-0">List of Candidates</h4> 
+                            <h4 class="header-title mt-0">List of Candidates</h4>
 
                             <br>
                             <br>
 
                             <form action="{{ route('election.search') }}" method="GET">
                                 @csrf
-                                
-                                <input type="text" name="search" placeholder="Search by name" style="border: none; border-bottom: 1px solid black;">
+
+                                <input type="text" name="search" placeholder="Search by name"
+                                    style="border: none; border-bottom: 1px solid black;">
                                 <button type="submit" class="fas fa-search" value="Search"></button>
-                                
+
                             </form>
 
                             <br>
                             <br>
                             <br>
-                            
+
                             <form action="{{ route('election.filter') }}" method="GET">
                                 <div class="form-group">
                                     <label for="year">Filter by Year of Election:</label>
@@ -56,54 +69,75 @@
                             <div class="table-responsive dash-social">
                                 <table id="datatable" class="table">
                                     <thead class="thead-light">
-                                    <tr>
-                                        <th>Picture</th>
-                                        <th class="text-center">Name</th>
-                                        <th class="text-center">Faculty</th>
-                                        <th class="text-center">Year</th>                                                    
-                                        <th class="text-center">Course</th>
-                                        <th class="text-center">Manifesto</th>
-                                        <th class="text-center">Election Year</th>
-                                        <th >Action</th>
-                                    </tr><!--end tr-->
+                                        <tr>
+                                            <th>Picture</th>
+                                            <th class="text-center">Name</th>
+                                            <th class="text-center">Faculty</th>
+                                            <th class="text-center">Year</th>
+                                            <th class="text-center">Course</th>
+                                            <th class="text-center">Manifesto</th>
+                                            <th class="text-center">Election Year</th>
+                                            <th class="text-center">Action</th>
+                                        </tr>
+                                        <!--end tr-->
                                     </thead>
 
                                     <tbody>
-                                    @forelse($election as $election)
-                                    <tr>
-                                        <td><img src="{{ asset('uploads/candidates/'.$election->candidate_picture) }}" alt="candidate_picture" width="50" height="50"></td>
-                                        <td class="text-center">{{ $election->candidate_name}}</td>
-                                        <td class="text-center">{{ $election->candidate_faculty}}</td>
-                                        <td class="text-center">{{ $election->candidate_year}}</td>
-                                        <td class="text-center">{{ $election->candidate_course}}</td>
-                                        <td class="text-center">{{ $election->candidate_manifesto}}</td>
-                                        <td class="text-center">{{ $election->election_year}}</td>
-                                        <td>
-                                            <a href="{{route('election.show', $election->id)}}"> <button class="dripicons-preview" ><i></i></button></a>
-                                            <br>
+                                        @forelse($election as $election)
+                                            <tr>
+                                                <td><img src="{{ asset('uploads/candidates/' . $election->candidate_picture) }}"
+                                                        alt="candidate_picture" width="50" height="50"></td>
+                                                <td class="text-center">{{ $election->candidate_name }}</td>
+                                                <td class="text-center">{{ $election->candidate_faculty }}</td>
+                                                <td class="text-center">{{ $election->candidate_year }}</td>
+                                                <td class="text-center">{{ $election->candidate_course }}</td>
+                                                <td class="text-center">{{ $election->candidate_manifesto }}</td>
+                                                <td class="text-center">{{ $election->election_year }}</td>
+                                                <td>
 
-                                            <a href="{{route('election.edit', $election->id)}}"> <button class="dripicons-document-edit" ><i></i></button></a>
-                                            <br>
+                                                    @if (Auth::user()->user_type == 'Student')
+                                                        <div class="card" style="display: none">
+                                                            <a href="{{ route('election.edit', $election->id) }}">
+                                                                <button
+                                                                    class="dripicons-document-edit"><i></i></button></a>
+                                                            <br>
 
-                                            <form action="{{route('election.destroy', $election->id)}}" method="POST">
-                                                @csrf
-                                                @method('DELETE')                                
-                                                <button type="submit" button class="dripicons-trash" onclick="return confirm('Are you sure you want to delete this candidate?')"></button>
-                                                {{-- <a href="{{route('election.destroy', $election->id)}}" onclick="return confirm('Are you sure you want to delete this candidate?')"><i class="fas fa-trash-alt text-danger font-16"></i></a> --}}
-                                            </form>                                    
-                                        </td>
-                                    </tr><!--end tr-->
-                                    @empty
-                                        <p class="text-warning">No candidate registered</p>
-                                    @endforelse
-                                                                                       
-                                    </tbody>
-                                </table>                    
-                            </div>                                         
-                        </div><!--end card-body--> 
-                    </div><!--end card--> 
-                </div> <!--end col-->                               
-            </div><!--end row--> 
+                                                            <form
+                                                                action="{{ route('election.destroy', $election->id) }}"
+                                                                method="POST">
+                                                                @csrf
+                                                                @method('DELETE')
+                                                                <button type="submit" button class="dripicons-trash"
+                                                                    onclick="return confirm('Are you sure you want to delete this candidate?')"></button>
+                                                            </form>
+                                                        @else
+                                                            <div class="card" style="align-items: center">
+                                                                <a href="{{ route('election.show', $election->id) }}">
+                                                                    <button
+                                                                        class="dripicons-preview"><i></i></button></a>
+                                                            </div>
+                                                    @endif
+                            </div>
+
+
+                            </td>
+                            </tr>
+                            <!--end tr-->
+                        @empty
+                            <p class="text-warning">No candidate registered</p>
+                            @endforelse
+
+                            </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <!--end card-body-->
+                </div>
+                <!--end card-->
+            </div>
+            <!--end col-->
         </div>
+        <!--end row-->
+    </div>
     </div>
 </x-app-layout>
